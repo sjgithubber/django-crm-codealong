7bc0b535d8483ab6d92cec67b8777b86bd5fe34f
sjgithubber <shubham991jain@gmail.com>
2025-04-03 21:41:01 +0530

    added registration page
    need to add proper user registration system other than the default one
    in django which makes admin create and manage all user accounts.

--- website/urls.py
+++ website/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", views.home, name='home'), 
     # path("login/", views.login_user ,name='login'),
     path("logout/", views.logout_user, name="logout"),
+    path("register/", views.register_user, name="register_user"),
     # path('home', views.home, name='home1')
 ]
--- website/views.py
+++ website/views.py
@@ -42,3 +42,6 @@ def logout_user(request):
     logout(request)
     messages.success(request,"You have been logged out!")
     return redirect('home')
+
+def register_user(request):
+    return render(request, 'register_user.html', {})
