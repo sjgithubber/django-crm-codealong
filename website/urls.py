1397c968b330bac838bc562faf2a998d4b2f1720
sjgithubber <shubham991jain@gmail.com>
2025-04-06 18:06:01 +0530

    delete record implementation for logged in users
    so far delete has been implemented, but next will implement it only for
    super users so that not anyone could delete records

--- website/urls.py
+++ website/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("logout/", views.logout_user, name="logout"),
     path("register/", views.register_user, name="register_user"),
     path("record/<int:pk>", views.record_data, name="record"),
+    path("delete_record/<int:pk>", views.delete_record, name="delete_record"),
     # path('home', views.home, name='home1')
 ]
--- website/views.py
+++ website/views.py
@@ -89,3 +89,13 @@ def record_data(request, pk):
     else:
         messages.success(request, "You must be logged in to view that page!")
         return redirect('home')
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:    
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "The user_record was successfully deleted")
+        return redirect('home')
+    else:
+        messages.info(request, "You are not logged in!")
+        return redirect('home')
